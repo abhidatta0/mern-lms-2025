dd377b86903829a736e2ee08c8f3f2d0ca8504c5
abhidatta0 <abhidatta146@gmail.com>
2025-10-25 11:04:20 +0530

    Multer:  delete files uploaded with multer once they are stored in the cloud?

--- server/routes/instructor/index.js
+++ server/routes/instructor/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const multer = require('multer');
 const {deleteMediaFromCloudinary, uploadMediaToCloudinary} = require('../../helpers/cloudinary');
-
+const fs = require('fs');
 const router = express.Router();
 
-const upload = multer({dest:'uploads/'});
+const upload = multer({dest:'./uploads'});
 
 router.post('/upload', upload.single('file'), async (req, res)=>{
     try{
@@ -12,6 +12,17 @@ router.post('/upload', upload.single('file'), async (req, res)=>{
      res.status(200).json({success: true, data: result})
     }catch(e){
         res.status(500).json({success: false, message:'Error Uploading'})
+    }finally{
+        // Stack overflow: https://stackoverflow.com/questions/64075741/how-to-delete-files-uploaded-with-multer-once-they-are-stored-in-the-cloud
+        let resultHandler = function (err) {
+        if (err) {
+            console.log("unlink failed", err);
+        } else {
+            console.log("file deleted");
+        }
+        }
+
+        fs.unlink(req.file.path, resultHandler);
     }
 });
 
